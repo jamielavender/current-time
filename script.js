952ef4cad55c06dad891166ecb81d8df68adda27
Jamie Lavender <77651206+jamielavender@users.noreply.github.com>
2024-12-07 08:42:28 +1100

    v1

--- script.js
+++ script.js
@@ -2,52 +2,15 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 
 
-let date = new Date();
+let time = document.getElementById("time");
 
+function currentTime() {
+  var d = new Date();
+  var s = d.getSeconds();
+  var m = d.getMinutes();
+  var h = d.getHours();
+  time.textContent = 
+    ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+};
 
-setInterval(getCurrent, 1000)
-
-
-let year = date.getFullYear();
-let month = date.getMonth() + 1;
-let day = date.getDate();
-let hours = date.getHours();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds();
-
-
-console.log(year);
-console.log(month);
-console.log(day);
-console.log(hours);
-console.log(minutes);
-console.log(seconds);
-
-function getCurrent() {
-    document.getElementById("yearText").innerHTML = year;
-    document.getElementById("monthText").innerHTML = month;
-    document.getElementById("dayText").innerHTML = day;
-    document.getElementById("hourText").innerHTML = hours;
-    document.getElementById("minuteText").innerHTML = minutes;
-    document.getElementById("secondText").innerHTML = seconds;
-}
-
-
-
-
-const yearText = document.querySelector("#yearText");
-const monthText = document.querySelector("#monthText");
-const dayText = document.querySelector("#dayText");
-
-
-function updateYear() {};
-
-function updateMonth() {};
-
-function updateDay() {};
-
-function updateHour() {};
-
-function updateMinute() {};
-
-function updateSecond() {};
+setInterval(currentTime, 1000);
